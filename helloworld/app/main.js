a575f2689deeb84eaa3419ade84c1438ee7da80a
Martin Kauss <yo@bishoph.org>
2014-10-31 12:46:19 +0100

    Step 6. Requesting an access token and sending this to the external web service for middleware authentication

--- helloworld/app/main.js
+++ helloworld/app/main.js
@@ -1,36 +1,57 @@
 define('helloworld/app/main', ['io.ox/core/extensions'], function () {
 
     'use strict';
-	
+
     var app = ox.ui.createApp({
         name: 'helloworld/app',
         title: 'Hello World App' // Can't be empty! This title appears if the user clicks on the App Launcher so it should be the same like in the manifest.json file
     });
 
     app.setLauncher(function () {
-	
+
 		console.log('hello world launcher init ...');
-	
+
         var win = ox.ui.createWindow({
             name: 'helloworld/app'
         });
         app.setWindow(win);
-		
-		var content =   $('<iframe>', { src: 'https://dev.ox.io/helloworld-app-service/', frameborder: 0 });
-		content.css({
-			width: '100%',
-            height: '100%'
-        });
-		
-        win.nodes.main.append(content);
 
-		console.log('... done');
-		
-        win.show();
+		/*
+			Acquire Token for external service to create a valid session for service authentication.
+
+ 			More details:
+			http://oxpedia.org/wiki/index.php?title=HTTP_API#Module_.22token.22_.28since_7.4.0.29
+		*/
+
+		$.getJSON( '/appsuite/api/token?action=acquireToken&session=' + ox.session, function( data ) {
+		  $.each( data, function( key, value ) {
+			if (key == 'data') {
+				$.each( value, function( dkey, dvalue ) {
+					var access_token = dvalue;
+					console.log('got acess_token = '+access_token);
+					init_window_and_show(win, access_token);
+				 });
+			}
+		  });
+		});
+
     });
-    
+
     return { getApp: app.getInstance };
-	
+
 });
 
+function init_window_and_show(win, access_token) {
+	var content =   $('<iframe>', { src: 'https://dev.ox.io/helloworld-app-service/?ox_token='+access_token, frameborder: 0 });
+	content.css({
+		width: '100%',
+		height: '100%'
+	});
+
+	win.nodes.main.append(content);
+
+	console.log('... done');
+
+	win.show();
+}
 
